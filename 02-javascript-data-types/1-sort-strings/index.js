ebcf91da5aaa9be9a1dd3de36e9e06b82b4b9752
Marat Ibragimov <Marat.Ibragimov@infowatch.com>
2025-01-24 16:09:36 +0300

    add 2-1 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,26 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  const ascComparator = (a, b) => a.localeCompare(b);
 
+  const descComparator = (a, b) => b.localeCompare(a);
+
+  const upperCaseComparator = (a, b) => {
+    if (a.toLowerCase() !== b.toLowerCase()) {
+      return 0;
+    }
+
+    if (a[0].toUpperCase() === a[0]) {
+      return -1;
+    }
+
+    if (b[0].toUpperCase() === b[0]) {
+      return 1;
+    }
+  };
+
+  return [...arr]
+    .sort(param === "asc" ? ascComparator : descComparator)
+    .sort(upperCaseComparator);
 }
