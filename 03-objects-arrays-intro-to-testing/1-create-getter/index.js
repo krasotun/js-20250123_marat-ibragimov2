2463fc295538593cc52ec4f5d628e957f3d247a3
Marat Ibragimov <Marat.Ibragimov@infowatch.com>
2025-01-27 10:47:59 +0300

    add 3-1 task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,25 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split(".");
 
+  const getter = (obj) => {
+    let value = obj;
+
+    for (const prop of props) {
+      if (value[prop] === undefined || typeof value[prop] === "function") {
+        return undefined;
+      }
+
+      if (value[prop] === null) {
+        return null;
+      }
+
+      value = value[prop];
+    }
+
+    return value;
+  };
+
+  return getter;
 }
