fd0f022f2b0af7603d19f2795d44c8ee097aa7c6
Marat Ibragimov <Marat.Ibragimov@infowatch.com>
2025-02-22 13:12:32 +0300

    add tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,69 @@
 class Tooltip {
-  initialize () {
+  static instance;
+  element = null;
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  initialize() {
+    this.createListeners();
+  }
+
+  createElement(textContent) {
+    this.element = document.createElement("div");
+    this.element.classList.add("tooltip");
+    this.element.textContent = textContent;
+  }
+
+  render(template) {
+    this.element = document.createElement("div");
+    this.element.innerHTML = template;
+    document.body.append(this.element);
+  }
+
+  createListeners() {
+    document.addEventListener("pointerover", this.handleDocumentPointerover);
+    document.addEventListener("pointerout", this.handleDocumentPointerout);
+  }
+
+  removeListeners() {
+    document.removeEventListener("pointerover", this.handleDocumentPointerover);
+    document.removeEventListener("pointerout", this.handleDocumentPointerout);
+  }
+
+  handleDocumentPointerover = (evt) => {
+    console.log("over", evt.target);
+
+    const shouldShowToolip = !!evt.target.dataset.tooltip;
+
+    if (shouldShowToolip) {
+      if (this.element) {
+        this.element.remove();
+      }
+
+      this.createElement(evt.target.dataset.tooltip);
+      evt.target.append(this.element);
+    }
+  };
+
+  handleDocumentPointerout = () => {
+    if (this.element) {
+      this.element.remove();
+    }
+  };
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.removeListeners();
   }
 }
 
